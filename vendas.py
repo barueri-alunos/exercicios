7185a0d6029e5d4b00fa949b3829043ebf2f912f
diandra <di.andrade90@gmail.com>
2019-12-06 11:54:49 -0300

    Change exercise

--- vendas.py
+++ vendas.py
@@ -4,9 +4,12 @@ def ganhar_comissao(vendas):
     if vendas == 10:
         salario1 = salario + 200
         return f'Vc vai receber {salario1}'
-    elif vendas > 10:
+    elif vendas > 10 and vendas < 25:
         salario2 = salario + 300 + (salario/2)
         return f'Vc vai receber {salario2}'
+    elif vendas >= 25:
+        salario3 = salario * 2
+        return f'Vc vai receber {salario3} e será promovido'
     else:
         return 'Obrigado'
 
